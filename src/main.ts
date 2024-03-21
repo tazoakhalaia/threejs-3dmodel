6e9a142e23f417852149a7182969e9743a96415a
tazoakhalaia <darknemiroff@gmail.com>
2024-03-21 23:06:42 +0400

    update code

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,11 @@
 import { GameScene } from "./game.class";
 
 class GameComponent {
-  gameScene = new GameScene();
-  init() {
-    this.gameScene.init();
-    fetch("./src/main.ts")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.blob();
-      })
-      .then((blob) => {
-        console.log("MIME type:", blob.type);
-      })
-      .catch((error) => {
-        console.error("Error fetching file:", error);
-      });
-  }
+    gameScene = new GameScene()
+    init() {
+        this.gameScene.init()
+    }
 }
 
-const gameComponent = new GameComponent();
-gameComponent.init();
+const gameComponent = new GameComponent()
+gameComponent.init()
