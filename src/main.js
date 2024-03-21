009885e9f9d960e7d6cba8a21efc80f4cd03ee78
tazoakhalaia <darknemiroff@gmail.com>
2024-03-21 23:01:26 +0400

    update code

--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@ class GameComponent {
     gameScene = new GameScene();
     init() {
         this.gameScene.init();
+        fetch("./src/main.ts")
+            .then((response) => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.blob();
+        })
+            .then((blob) => {
+            console.log("MIME type:", blob.type);
+        })
+            .catch((error) => {
+            console.error("Error fetching file:", error);
+        });
     }
 }
 const gameComponent = new GameComponent();
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { GameScene } from "./game.class";
 
 class GameComponent {
-    gameScene = new GameScene()
-    init() {
-        this.gameScene.init()
-    }
+  gameScene = new GameScene();
+  init() {
+    this.gameScene.init();
+    fetch("./src/main.ts")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.blob();
+      })
+      .then((blob) => {
+        console.log("MIME type:", blob.type);
+      })
+      .catch((error) => {
+        console.error("Error fetching file:", error);
+      });
+  }
 }
 
-const gameComponent = new GameComponent()
-gameComponent.init()
+const gameComponent = new GameComponent();
+gameComponent.init();
