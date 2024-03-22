7b1d7ee234cd007ccc063f137c95a2a65b8ee2ab
tazoakhalaia <darknemiroff@gmail.com>
2024-03-22 23:07:09 +0400

    update code

--- src/game.class.ts
+++ src/game.class.ts
@@ -10,7 +10,7 @@ export class GameScene {
   mixerUpdate = 0.01;
   pos = 0;
   player: any;
-  loader = new THREE.TextureLoader().load('./model/gg.jpg')
+  loader = new THREE.TextureLoader().load('public/model/gg.jpg')
 
   init() {
     this.drawScene();
